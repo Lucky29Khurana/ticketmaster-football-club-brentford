5cdb621edd167b7a64eb3217130467efba3db128
Lucky29Khurana <lk@prospectsportinginsights.co.uk>
2024-09-17 11:49:54 +0530

    AI pipeline code completed

--- src/ticketmaster_football_club_brentford/pipelines/01_athena_ingestion/nodes.py
+++ src/ticketmaster_football_club_brentford/pipelines/01_athena_ingestion/nodes.py
@@ -4,100 +4,156 @@ generated using Kedro 0.19.6
 """
 from prospect.ticketmaster.athena_ingestion import *
 import logging
+
 logger = logging.getLogger(__name__)
 
 
 # NODES
 
 def load_and_format_athena_event_data(
-        params: dict,
-) -> pd.DataFrame:
+        ingestion_params: dict,
+) -> [pd.DataFrame, pd.DataFrame]:
+    """
+    Loads and formats match event data for the relevant client in athena. Matches are matched using Wyscout data for
+    football clients and Oval data for rugby clients.
 
-    if params['client_type'] == 'rugby_club':
-        # df =  load_and_format_oval_rugby_event_data(
-        #
-        # )
+    Parameters
+    ----------
+    ingestion_params: dict
+        Dictionary used to identify relevant parameters and s3_staging_dir.
+
+    Returns
+    -------
+    pd.DataFrame
+        A dataframe containing event data from the Athena Table for the specific client's event
+
+    pd.DataFrame
+        A dataframe containing fixtures data from Snowflake for specific ticketmaster client
+
+    Raises
+    ------
+    Exception
+        If 'client_type' from 'ingestion_params' is a string of the type 'rugby_club', 'football_club',
+        and 'rugby_international'.
+    """
+    logger.info("Executing function to load and format event data.")
+
+    if ingestion_params['client_type'] == 'rugby_club':
         pass
-    elif params['client_type'] == 'football_club':
-        df = load_and_format_athena_football_event_data(
-            ticketmaster_client_name=params['client_athena_name'],
-            s3_staging_dir=params['s3_staging_dir'],
-            wyscout_client_team_id=params['client_wyscout_id'],
-            wyscout_client_team_name=params['client_wyscout_name'],
-            additional_events_filter_keywords=params['additional_events_filter_keywords'],
-            snowflake_region="",     # TODO: add snowflake credentials
-            snowflake_secret_key="",
+
+    elif ingestion_params['client_type'] == 'football_club':
+        event_df, fixtures_df = load_and_format_athena_football_event_data(
+            ticketmaster_client_name=ingestion_params['client_athena_name'],
+            s3_staging_dir=ingestion_params['s3_staging_dir'],
+            wyscout_client_team_id=int(ingestion_params['client_wyscout_id']),
+            wyscout_client_team_name=ingestion_params['client_wyscout_name'],
+            additional_events_filter_keywords=ingestion_params['additional_events_filter_keywords'],
+            snowflake_region=ingestion_params['snowflake_region'],
+            snowflake_secret_key=ingestion_params['snowflake_secret_key']
         )
-    elif params['client_type'] == 'rugby_international':
-        # df = load_and_format_oval_rugby_event_data(
-        #
-        # )
+
+        return event_df, fixtures_df
+
+    elif ingestion_params['client_type'] == 'rugby_international':
         pass
     else:
         raise Exception('Client type is invalid.')
 
-    logger.info('formatted_event_df successfully created.')
-
-    return df
+    logger.info('formatted_event_df and fixtures_df successfully created.')
 
 
 def load_and_format_transaction_data(
         formatted_event_df: pd.DataFrame,
-        params: dict,
+        ingestion_params: dict,
 ) -> pd.DataFrame:
+    """
+    Loads and formats transaction data for the given client. Transactions are filtered for match-days (as specified in
+    formatted_event_df) and season_ticket_ids.
 
-    event_ids = params['season_ticket_event_ids'].values() + formatted_event_df['event_data_id'].tolist()
+    Parameters
+    ----------
+    formatted_event_df: pd.DataFrame
+        A dataframe with formatted information for each match-day event for the given client.
+    ingestion_params: dict
+        Dictionary used to identify season_ticket_event_ids and the client_athena_name and s3_staging_dir..
 
-    df = load_and_format_athena_transaction_data(
-        ticketmaster_client_name=params['client_athena_name'],
-        event_ids=event_ids
+    Returns
+    -------
+    pd.DataFrame
+        A dataframe with formatted transaction information for each match-day event / season ticket purchase for the
+        given client.
+
+        """
+    logger.info("Executing function to load transaction data.")
+
+    logger.info("Extracting relevant event ids for which we need to extract Transactions Data")
+    event_ids = ingestion_params['season_ticket_event_ids'].values() + formatted_event_df['event_data_id'].tolist()
+
+    transactions_df = load_and_format_athena_transaction_data(
+        ticketmaster_client_name=ingestion_params['client_athena_name'],
+        s3_staging_dir=ingestion_params['s3_staging_dir'],
+        event_ids=event_ids,
     )
 
-    logger.info('all_transactions_df successfully created.')
+    logger.info("formatted_transactions_df successfully created.")
 
-    return df
+    return transactions_df
 
 
-def load_and_format_member_data(
-        params: dict,
+def load_member_data(
+        ingestion_params: dict,
 ) -> pd.DataFrame:
+    logger.info("Executing function to load TM Members Data from Athena")
 
-    df = load_athena_member_data(
-        ticketmaster_client_name=params['client_athena_name']
+    members_df = load_athena_member_data(
+        ticketmaster_client_name=ingestion_params['client_athena_name'],
+        s3_staging_dir=ingestion_params['s3_staging_dir'],
     )
 
-    logger.info('all_members_df successfully created.')
+    logger.info("Members Data loaded successfully from Athena")
 
-    return df
+    return members_df
 
 
 def load_and_format_season_card_data(
-        params: dict,
+        ingestion_params: dict,
 ) -> pd.DataFrame:
+    logger.info("Executing function to load and format TM Season Card Data from Athena")
 
-    df = load_and_format_season_card_data(
-        ticketmaster_client_name=params['client_athena_name']
+    season_card_df = load_and_format_season_card_data(
+        ticketmaster_client_name=ingestion_params['client_athena_name'],
+        s3_staging_dir=ingestion_params['s3_staging_dir'],
     )
 
-    # TODO: make sure that event_name column is proper
+    # renaming values of event_name column to relevant values
+    season_card_df['event_name'] = season_card_df['event_name'].replace({
+        'Season 2023\\/24': 'Season 2023/24',
+        'Season 2024\\/25': 'Season 2024/25',
+    })
 
-    logging.info('formatted_season_card_df successfully created.')
+    # dropping rows with irrelevant events
+    season_card_df = season_card_df[~(season_card_df['event_name'] == 'Test Season 23\/24')]
 
-    return df
+    logging.info("Formatted Season Card Data loaded successfully from Athena")
+
+    return season_card_df
 
 
 def load_and_format_ticketscan_data(
         formatted_event_df: pd.DataFrame,
-        params: dict,
+        ingestion_params: dict,
 ) -> pd.DataFrame:
+    logger.info("Executing function to load and format TM Ticket Scan Data from Athena")
 
-    event_ids = formatted_event_df['event_data_id'].tolist()
+    event_ids = formatted_event_df[formatted_event_df['competition_name'].isin(ingestion_params['competition_name'])][
+        'event_data_id'].unique().tolist()
 
-    df = load_and_format_athena_ticketscan_data(
-        ticketmaster_client_name=params['client_athena_name'],
-        league_matches_event_ids=event_ids
+    ticketscan_df = load_and_format_athena_ticketscan_data(
+        ticketmaster_client_name=ingestion_params['client_athena_name'],
+        s3_staging_dir=ingestion_params['s3_staging_dir'],
+        league_matches_event_ids=event_ids,
     )
 
-    logger.info('formatted_ticketscan_df successfully created.')
+    logger.info("Formatted Ticket Scan Data loaded successfully from Athena")
 
-    return df
+    return ticketscan_df
--- src/ticketmaster_football_club_brentford/pipelines/01_athena_ingestion/pipeline.py
+++ src/ticketmaster_football_club_brentford/pipelines/01_athena_ingestion/pipeline.py
@@ -4,13 +4,7 @@ generated using Kedro 0.19.6
 """
 
 from kedro.pipeline import Pipeline, pipeline, node
-from .nodes import (
-    load_and_format_athena_event_data,
-    load_and_format_transaction_data,
-    load_and_format_member_data,
-    load_and_format_season_card_data,
-    load_and_format_ticketscan_data
-)
+from .nodes import *
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -19,8 +13,16 @@ def create_pipeline(**kwargs) -> Pipeline:
             node(
                 func=load_and_format_athena_event_data,
                 inputs="params:ingestion_params",
-                outputs="formatted_event_df",
-                name="formatted_event_df_node"
+                outputs=[
+                    "formatted_event_df",
+                    "fixtures_df",
+                ],
+                name="load_and_format_athena_event_data_node",
+                tags=[
+                    'football_club_ai_pipeline',
+                    'rugby_club_ai_pipeline',
+                    'rugby_international_ai_pipeline'
+                ],
             ),
 
             node(
@@ -29,22 +31,37 @@ def create_pipeline(**kwargs) -> Pipeline:
                     "formatted_event_df",
                     "params:ingestion_params"
                 ],
-                outputs="all_transactions_df",
-                name="load_athena_member_data_node"
+                outputs="formatted_transactions_df",
+                name="load_and_format_transaction_data_node",
+                tags=[
+                    'football_club_ai_pipeline',
+                    'rugby_club_ai_pipeline',
+                    'rugby_international_ai_pipeline'
+                ]
             ),
 
             node(
-                func=load_and_format_member_data,
+                func=load_member_data,
                 inputs="params:ingestion_params",
-                outputs="all_members_df",
-                name="load_and_format_member_data_node"
+                outputs="members_df",
+                name="load_and_format_member_data_node",
+                tags=[
+                    'football_club_ai_pipeline',
+                    'rugby_club_ai_pipeline',
+                    'rugby_international_ai_pipeline'
+                ]
             ),
 
             node(
                 func=load_and_format_season_card_data,
                 inputs="params:ingestion_params",
                 outputs="formatted_season_card_df",
-                name="load_and_format_member_data_node"
+                name="load_and_format_season_card_data_node",
+                tags=[
+                    'football_club_ai_pipeline',
+                    'rugby_club_ai_pipeline',
+                    'rugby_international_ai_pipeline'
+                ]
             ),
 
             node(
@@ -54,7 +71,12 @@ def create_pipeline(**kwargs) -> Pipeline:
                     "params:ingestion_params"
                 ],
                 outputs="formatted_ticketscan_df",
-                name="load_and_format_ticketscan_data_node"
+                name="load_and_format_ticketscan_data_node",
+                tags=[
+                    'football_club_ai_pipeline',
+                    'rugby_club_ai_pipeline',
+                    'rugby_international_ai_pipeline'
+                ]
             )
         ]
     )
